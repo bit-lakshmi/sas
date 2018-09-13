68ca68952f47b26037d0d51ab94ead335c901f9b
A6i8 <amtecworld76652@gmail.com>
2018-09-13 20:03:45 +0530

    13-9-18

--- data/js-function.js
+++ data/js-function.js
@@ -98,7 +98,7 @@ var sttable = '';
                         sPrevious: '<i class="material-icons">chevron_left</i>',
                         sNext: '<i class="material-icons">chevron_right</i>',
                         sLast: '<i class="material-icons">chevron_right</i>'
-                }
+                    }
                 }
             });
             $('.dataTables_length select').addClass('browser-default');
